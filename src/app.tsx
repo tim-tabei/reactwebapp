b6bf9e153721622d3bc2841dc650419aed28b184
tim-tabei <t_tabei@timedia.co.jp>
2025-05-20 22:21:34 +0900

    中身変更

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,8 @@
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!!!</h1>
-      <p>This is a simple React application that uses TypeScript and Vite.</p>
+      <h1>田部井登輝のぺーじだよ</h1>
+      <p>ドメイン変えたいけどやり方わからん</p>
     </div>
   );
 }
