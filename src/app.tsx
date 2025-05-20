efe32cf7ec19bb42e5056bd9b737162ff3a4a644
tim-tabei <t_tabei@timedia.co.jp>
2025-05-20 22:07:47 +0900

    add message

--- src/app.tsx
+++ src/app.tsx
@@ -2,6 +2,7 @@ function App() {
   return (
     <div className="App">
       <h1>Hello World!!!</h1>
+      <p>This is a simple React application that uses TypeScript and Vite.</p>
     </div>
   );
 }
